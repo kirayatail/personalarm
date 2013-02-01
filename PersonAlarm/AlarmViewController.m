f51d00ef9ac457fcd093727d4be4e1ede7894ab0
Max Witt <kirayatail@gmail.com>
2013-02-01 10:21:12 +0100

    Created an XCode project with a single VC, ARC and Storyboards for iPhone

---
+++ PersonAlarm/AlarmViewController.m
@@ -0,0 +1,29 @@
+//
+//  AlarmViewController.m
+//  PersonAlarm
+//
+//  Created by Max Witt on 2013-02-01.
+//  Copyright (c) 2013 Chalmers. All rights reserved.
+//
+
+#import "AlarmViewController.h"
+
+@interface AlarmViewController ()
+
+@end
+
+@implementation AlarmViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
