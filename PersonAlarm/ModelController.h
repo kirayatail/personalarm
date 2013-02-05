7c1b6a964c0b7dbc594823de568f58f77cb44f47
simon olsson <olssonsim@gmail.com>
2013-02-05 12:53:51 +0100

    Created a base for a page based application

---
+++ PersonAlarm/ModelController.h
@@ -0,0 +1,20 @@
+//
+//  ModelController.h
+//  PersonAlarm
+//
+//  Created by Simon Olsson on 2013-02-05.
+//  Copyright (c) 2013 Chalmers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ModelController : NSObject<UIPageViewControllerDataSource>
+
+- (UIViewController *) viewControllerAtIndex:(NSUInteger)index
+                                  storyboard:(UIStoryboard *) storyboard;
+
+- (NSUInteger) indexOfViewController:(UIViewController *) viewController;
+
+- (id) initWithStoryboard:(UIStoryboard *) storyboard;
+
+@end
