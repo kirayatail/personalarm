db0c3daab25850d6c51f8b7a55b815795d1c027e
Max Witt <kirayatail@gmail.com>
2013-02-05 09:34:54 +0100

    Created a controller interfacing towards alarm methods

---
+++ PersonAlarm/AlarmController.m
@@ -0,0 +1,24 @@
+/*
+//  AlarmController.m
+//  PersonAlarm
+//
+//  Created by Max Witt on 2013-02-05.
+//  Copyright (c) 2013 Chalmers. All rights reserved.
+//
+
+ This class acts as an interface between the view controllers and the alarm methods. 
+ 
+*/
+
+#import "AlarmController.h"
+
+@implementation AlarmController
+-(void)triggerAlarm {
+    // Start each chosen alarm
+}
+
+-(void)stop {
+    // Stops any running alarm and disables triggering of the next alarm
+}
+
+@end
