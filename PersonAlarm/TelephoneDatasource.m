1d6cc6d80ba66cbeffaaac51688c8c23ccb162f6
siimonma <martinsson.challe@me.com>
2013-02-05 10:48:47 +0100

    init method gets value for key 'Number' in UserDefault

--- PersonAlarm/TelephoneDatasource.m
+++ PersonAlarm/TelephoneDatasource.m
@@ -16,6 +16,18 @@
 
 @synthesize phonenumber = _phonenumber;
 
+-(id)init {
+    
+    // Get phonenumber from userdefault
+    if (self = [super init]){
+        NSUserDefaults *numbers = [NSUserDefaults standardUserDefaults];
+        _phonenumber = [numbers objectForKey:@"Number"];
+        
+    }
+    
+    return self;
+}
+
 -(void) setPhonenumber:(NSString *)phonenumber {
     NSUserDefaults *numbers = [NSUserDefaults standardUserDefaults];
     
