86e5f687a5b50af52b43c68661c28c2e3ac4f321
Max Witt <kirayatail@gmail.com>
2013-02-05 10:36:33 +0100

    Implemented the TelephoneAlarm, using a single number and owning its data source

--- PersonAlarm/TelephoneAlarm.m
+++ PersonAlarm/TelephoneAlarm.m
@@ -7,7 +7,21 @@
 //
 
 #import "TelephoneAlarm.h"
+#import "TelephoneDatasource.h"
+@interface TelephoneAlarm ()
+@end
 
 @implementation TelephoneAlarm
+@synthesize datasource = _datasource;
 
+-(void)trigger {
+    // TODO: check if datasource content is valid
+    NSString *number = self.datasource.phonenumber;
+    
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:[@"tel://" stringByAppendingString:number]]];
+}
+
+-(void)stop {
+    // Do nothing, this is not a continuous alarm.
+}
 @end
