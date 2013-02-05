0be8b41ec7a3472b23da12de9a5141b503791f52
Max Witt <kirayatail@gmail.com>
2013-02-05 09:34:54 +0100

    Created a controller interfacing towards alarm methods

---
+++ PersonAlarm/AlarmController.h
@@ -0,0 +1,15 @@
+//
+//  AlarmController.h
+//  PersonAlarm
+//
+//  Created by Max Witt on 2013-02-05.
+//  Copyright (c) 2013 Chalmers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AlarmController : NSObject
+-(void)triggerAlarm;
+
+-(void)stop;
+@end
