703fd9b0a1dab255203119dd89ae72f2914d92c9
simon olsson <olssonsim@gmail.com>
2013-02-13 17:30:28 +0100

    Added profile and friends settings to the settings list

---
+++ ProfileViewController.h
@@ -0,0 +1,23 @@
+//
+//  ProfileViewController.h
+//  PersonAlarm
+//
+//  Created by Simon Olsson on 2013-02-13.
+//  Copyright (c) 2013 Chalmers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ProfileViewController;
+
+@protocol ProfileViewControllerDelegate <NSObject>
+
+- (void) profileViewController:(ProfileViewController *) profileViewController didPressDone:(BOOL) pressed;
+
+- (void) profileViewController:(ProfileViewController *) profileViewController didPressCancel:(BOOL) pressed;
+
+@end
+
+@interface ProfileViewController : UIViewController
+
+@end
---
+++ ProfileViewController.m
@@ -0,0 +1,23 @@
+//
+//  ProfileViewController.m
+//  PersonAlarm
+//
+//  Created by Simon Olsson on 2013-02-13.
+//  Copyright (c) 2013 Chalmers. All rights reserved.
+//
+
+#import "ProfileViewController.h"
+
+@interface ProfileViewController ()
+
+@end
+
+@implementation ProfileViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+@end
