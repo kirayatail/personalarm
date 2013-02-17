5ba6528b49a4d2a22dc23ed8f75dfc7a772c113a
William <w.gabrielsson@gmail.com>
2013-02-17 16:16:17 +0100

    Cleanup in SettingsTBV, now the segues are working properly

--- PersonAlarm/SettingsTableViewController.m
+++ PersonAlarm/SettingsTableViewController.m
@@ -11,12 +11,6 @@
 
 @interface SettingsTableViewController ()
 
-#define SETTINGS_SECTION 0
-#define GENERAL_SECTION 1
-#define MESSAGE_SETTINGS 0
-#define CALL_SETTINGS 1
-#define PROFILE_SETTINGS 0
-#define FRIEND_SETTINGS 1
 
 @property (nonatomic, strong) TelephoneDatasource *datasource;
 
@@ -42,7 +36,6 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
-    self.tableView.dataSource = self;
 }
 
 #pragma mark MessageSettingsViewController delegate methods
@@ -89,27 +82,6 @@
     }];
 }
 
-#pragma mark -
 
--(void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if(indexPath.row == MESSAGE_SETTINGS && indexPath.section == SETTINGS_SECTION) {
-        [self performSegueWithIdentifier:@"Show Message Settings" sender:self];
-    } else if(indexPath.row == CALL_SETTINGS && indexPath.section == SETTINGS_SECTION) {
-        [self performSegueWithIdentifier:@"Show Call Settings" sender:self];
-    } else if (indexPath.row == PROFILE_SETTINGS && indexPath.section == GENERAL_SECTION) {
-        [self performSegueWithIdentifier:@"Show Profile Settings" sender:self];
-    } else if (indexPath.row == FRIEND_SETTINGS && indexPath.section == GENERAL_SECTION) {
-        // Add the view controller and segue for friend settings!!
-        [self performSegueWithIdentifier:@"Show Friend Settings" sender:self];
-    }
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    // Return the number of rows in the section.
-    
-    return 2;
-}
 
 @end
